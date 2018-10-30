c7993b0caccfe783bcae947326b4dce09b5c4c38
i65 <2461585309@qq.com>
2018-10-30 15:15:20 +0000

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,22 @@
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+
+// Auth::routes(); 为了直观，替换为下面的路由
+
+//用户认证路由
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+//注册路由
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+//重置密码路由
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
