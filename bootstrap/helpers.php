d9861af41a89e0fa721a3ac41695cc9871b9fbda
i65 <2461585309@qq.com>
2018-11-09 09:16:08 +0000

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,10 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 生成excerpt字段内容
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', '', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
