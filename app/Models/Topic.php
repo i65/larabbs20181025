9cf24925c470274b9059cfb0df8bf5db4fff0c4b
i65 <2461585309@qq.com>
2018-11-14 06:55:12 +0000

    List user replies

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,6 +6,10 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
     //一个话题属于一个分类
     public function category()
     {
